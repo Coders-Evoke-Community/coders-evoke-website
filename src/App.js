a449023e0cb18c2ffe8904aa4e918ef13662f619
Avnish Bharadva <avnishbharadva@gmail.com>
2022-05-25 14:33:39 +0530

    About Section Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 // import Navbar from './components/navbar/navbar'
 import Hero from './components/hero/hero'
+import About from './components/about/about'
 
 function App() {
   return (
     <>
     {/* <Navbar/> */}
     <Hero/>
+    <About/>
     </>
   );
 }
